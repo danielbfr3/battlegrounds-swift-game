e3b15ec762313015387c61e94a59e75fc50c1ba9
Daniel Freire <danielfr3@me.com>
2016-05-06 20:21:51 -0300

    Initial Commit

---
+++ battlegrounds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  battlegrounds
+//
+//  Created by Daniel Boga on 06/05/16.
+//  Copyright © 2016 Daniel Boga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
